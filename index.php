76ce9a961c11c641a3143e805e54b0a18e8312a6
arman <arman.antonyan12@gmail.com>
2018-01-20 01:19:41 +0400

    only about nt btn

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="https://fonts.googleapis.com/css?family=Titillium+Web:200" rel="stylesheet">
+
+	<link href="https://fonts.googleapis.com/css?family=Titillium+Web:300" rel="stylesheet">
+	
+	<link href="https://fonts.googleapis.com/css?family=Titillium+Web" rel="stylesheet">
+
+	<!--<link rel="stylesheet" href="css/bootstrap.min.css"  crossorigin="anonymous">-->
+
+	<!-- <link rel="stylesheet" href="css/bootstrap-grid.min.css"  crossorigin="anonymous">
+
+	<script src="js/jquery-3.1.1.min.js"  crossorigin="anonymous"></script>
+
+	<script src="js/tether.min.js" crossorigin="anonymous"></script>
+
+	<script src="js/bootstrap.min.js"  crossorigin="anonymous"></script>
+	-->
+</head>
+<body>
+
+<div id="home">
+	<script src="js/three.min.js"></script>
+	<script src="js/Projector.js"></script>
+	<script src="js/CanvasRenderer.js"></script>
+	<script src="js/script.js"></script>
+  <header>
+	<div class="menu">
+
+		<nav class="menu_logo">
+						
+		</nav>
+		<nav class="menu_r">
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">About</a></li>
+					<li><a href="#">Portfolio</a></li>
+					<li><a href="#">team</a></li>
+					<li><a href="#">Contact</a></li>
+					
+				</ul>	
+		</nav>
+	   </div>	
+   </header>
+</div>
+<div id="about">
+	<h3 class="page_title">About</h3>
+	<div class="row">
+			<div class="right_logo" >
+				<img src="img/logo/logo.png" width="256">
+			</div>
+			<div class="left_about">
+			  <p style="color:#FFFFFF;">
+				Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+			
+			
+			</p>
+			</div>
+	</div>
+</div>
+</body>
+</html>
